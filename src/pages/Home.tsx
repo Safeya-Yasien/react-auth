8132e2c0798365ac9c0b45e46af8bb12b3eecc41
Safeya-Yasien <ssafeya5@gmail.com>
2024-11-01 13:30:10 +0200

    feat: create Home.tsx

---
+++ src/pages/Home.tsx
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <div className="flex flex-col justify-center items-center ">
+      <h1 className="text-2lg font-bold p-12">Home</h1>
+    </div>
+  );
+};
+
+export default Home;
