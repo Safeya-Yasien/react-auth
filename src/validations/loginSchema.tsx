f53df4733e30a859be6feb43821effcb9b5c2a72
Safeya-Yasien <ssafeya5@gmail.com>
2024-11-02 08:13:34 +0200

    update loginSchema delete pass regex not important in login only in register

--- src/validations/loginSchema.tsx
+++ src/validations/loginSchema.tsx
@@ -5,10 +5,7 @@ export const loginSchema = z.object({
   password: z
     .string()
     .min(8, { message: "Password must be at least 8 characters long" })
-    .max(30, { message: "Password must not exceed 30 characters" })
-    .regex(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d@$!%*?&]{8,30}$/, {
-      message: "Password must contain at least one letter and one number",
-    }),
+    .max(30, { message: "Password must not exceed 30 characters" }),
   remember_me: z.boolean(),
 });
 
